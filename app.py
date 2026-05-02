a503fe83deeb73e0792b861b574c1226e062c902
adithii90 <adithiikannan90@gmail.com>
2026-05-02 08:34:45 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import random 
+st.title("Rock Paper Scissor GAME")
+st.subheader("Created by adithii")
+st.write("You will be playing rock paper scisscor with the computer.Enter your choice in the box and may the best win!")
+
+
+
+title = st.text_input(label="Enter Input",value= "Rock, Paper, scissors")
+a=random.choice(["rock","paper","scissors"])
+
+if st.button("go"):
+    if title==a:
+        st.info("It's draw! ")
+    else : 
+        if title=="rock" and a=="scissors" : 
+            st.success("you won!")
+        elif title=="scissors" and a=="paper" :
+            st.success("You won!")
+        elif title=="paper" and a=="rock" :
+            st.success("you won!")
+        else :
+            st.warning("You lose!")
+    st.write("You have put "+ str(title))
+    st.write(" the computer has put "+ str(a))
